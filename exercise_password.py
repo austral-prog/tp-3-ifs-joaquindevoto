bafa126f07a3577e6caa3f826d9d430471b14c5d
joaquindevoto <jdevoto08@gmail.com>
2026-03-27 15:22:26 -0300

    Update exercise_password.py

--- exercise_password.py
+++ exercise_password.py
@@ -23,4 +23,36 @@ def password():
         Contraseña muy corta
         Debe contener un numero
     """
+
+    contraseña= input()
+
+    if len(contraseña)>=8:
+        largo_valido= True
+    else:
+        largo_valido= False
+
+    if "1"in contraseña or "2"in contraseña or "3"in contraseña or "4"in contraseña or "5"in contraseña or "6"in contraseña or "7"in contraseña or "8"in contraseña or "9"in contraseña or "0"in contraseña:
+        numero_valido= True
+    else:
+        numero_valido= False
+
+    if largo_valido and numero_valido:
+        print("Contraseña valida")
+
+    elif not largo_valido and not numero_valido:
+        print("Contraseña muy corta\nDebe contener un numero")
+
+    elif not largo_valido and numero_valido:
+        print("Contraseña muy corta")
+
+    elif largo_valido and not numero_valido:
+        print("Debe contener un numero")
+
+
+
+
+        Para la entrada "abc", la salida esperada es:
+        Contraseña muy corta
+        Debe contener un numero
+    """
     pass
